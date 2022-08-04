5b03a425447816be1716a7b68f2c943f0f671870
bugraYvz99 <you@example.com>
2022-08-04 23:43:20 +0300

    New Edit button added. Task features are updated.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
-        
+
         windowKeyEnterPressed();
         userinput.value = "";
     }
-   
+
 }, false);
 
 const userinput = document.querySelector("#user-input");
@@ -14,9 +14,15 @@ const itemlist = document.querySelector(".items");
 
 function windowKeyEnterPressed() {
     //CREATE LİST
-    var li = document.createElement("li");
+    var li = document.createElement("input");
     li.classList.add("tasks")
+    li.type = "text";
+    li.value = userinput.value;
+    li.setAttribute("readonly", "readonly");
+
     //BUTTONS
+    var actions = document.createElement("div");
+    actions.classList.add("actions");
     var edit_btn = document.createElement("button");
     var delete_btn = document.createElement("button");
     delete_btn.classList.add("dltbtn");
@@ -24,36 +30,44 @@ function windowKeyEnterPressed() {
     edit_btn.innerText = "EDİT";
     delete_btn.innerText = "DELETE";
     var icon = document.createElement("i");
-    icon.classList.add("fa-solid","fa-trash");
+    icon.classList.add("fa-solid", "fa-trash");
     delete_btn.appendChild(icon);
-    
+    actions.appendChild(edit_btn);
+    actions.appendChild(delete_btn);
+
+
     //DELETE FUNCTİON
-    
-    delete_btn.onclick = function (){
-        
+
+    delete_btn.onclick = function () {
+
         itemlist.removeChild(li);
+        itemlist.removeChild(actions);
     }
     //EDİT FUNCTİON
-    edit_btn.onclick = function(e){
-       let selectedLi = e.path[1];
-       console.log
-       var inputValue = userinput.value;
-       userinput.value = "";
-        selectedLi.innerHTML = inputValue
-        selectedLi.appendChild(delete_btn);
-        selectedLi.appendChild(edit_btn);
-       
+    edit_btn.onclick = function (e) {
+        if (edit_btn.innerText == "EDİT") {
+            li.removeAttribute("readonly");
+            li.focus();
+            edit_btn.innerText = "SAVE";
+        }
+        else {
+            li.setAttribute("readonly", "readonly");
+            edit_btn.innerText = "EDİT"
+        }
+
+
     }
     // İNPUT VALUE TO LİST
     var inputValue = userinput.value;
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
-    li.appendChild(delete_btn);
-    li.appendChild(edit_btn);
+
     itemlist.appendChild(li);
-    
-    
-    
-    
+    itemlist.appendChild(actions);
+
+
+
+
+
 }
 
